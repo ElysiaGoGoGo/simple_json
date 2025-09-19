f50325a41910669213cc1e20704fbd68ae2c42a0
Baka <you@example.com>
2025-09-19 16:58:09 +0800

    delete files commited by mistake

--- cmake-build-linux_debug-/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baka/文档/code/simple_json/src
-# Build directory: /home/baka/文档/code/simple_json/cmake-build-linux_debug-/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-linux_debug-/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/baka/文档/code/simple_json/test
-# Build directory: /home/baka/文档/code/simple_json/cmake-build-linux_debug-/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
